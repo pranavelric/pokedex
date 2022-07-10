085ef402e53a8656b56991b75569c14672c917d4
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-07-10 15:16:07 +0530

    Heading Component added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './styles/App.scss';
+import PokemonProvider from './context/PokemonProvider';
+import Heading from './components/Heading/Heading';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <PokemonProvider>
+        <div className="pokedex_app">
+          <Heading/>
+
+        </div>
+      
+      </PokemonProvider>
+ );
 }
 
 export default App;
---
+++ src/components/Heading/Heading.jsx
@@ -0,0 +1,12 @@
+import './heading.scss';
+import React from 'react'
+
+export default function Heading() {
+	return (
+		<h1 className="heading">
+            P<i></i>kédex
+
+            
+		</h1>
+	);
+}
