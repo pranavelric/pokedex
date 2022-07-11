29d22124bb0187ee8d9b55d519dda309685e3f26
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-07-11 23:12:28 +0530

    Pokedex View added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import PokemonProvider from './context/PokemonProvider';
 import Heading from './components/Heading/Heading';
 import {withRouter} from './utils/withRouter';
 import Navigation from './components/Navigation/Navigation';
+import {Routes, Route, Navigate} from 'react-router-dom'
+import generations from './data/generations';
+import PokedexView from './components/PokedexView/PokedexView';
 
 function App() {
   return (
@@ -11,7 +14,16 @@ function App() {
         <div className="pokedex_app">
           <Heading/>
           <Navigation/>
-          
+            <Routes>
+                <Route exact path='/' element={ <Navigate  to= {generations[0].link} /> }/> 
+                {
+                  generations.map(({id,link})=>(
+               
+          <Route key={ id } exact path={ '/' + link } element={<PokedexView generation={id} />}/>
+                  ))
+                }
+            </Routes>
+
 
         </div>
       
