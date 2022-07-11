1618e0c5d6b173632217ffb6fd5f09d596767d46
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-07-11 23:11:57 +0530

    Pokedex view added

---
+++ src/components/PokedexView/PokedexView.jsx
@@ -0,0 +1,35 @@
+import './pokedexview.scss';
+import React from 'react'
+
+import { useEffect } from 'react';
+import { usePokemons } from '../../context/PokemonProvider';
+import useGeneration from '../../hooks/useGeneration';
+import Card from '../card/Card';
+import Loader from '../Loader/Loader';
+
+
+export default function PokedexView({generation}) {
+    const {pokemons,setPokemons,setCurrentPokemonId} = usePokemons();
+    const {data,isLoading}= useGeneration(generation);
+
+    useEffect(()=>{
+        setPokemons(data)
+    },[data]);
+
+    if(isLoading){
+        return <Loader/>
+    }
+
+  return (
+    <div className="pokedex_view">
+        {
+            pokemons.map((pokemon)=>{
+                return (
+                <Card key={pokemon.id} pokemon={pokemon} onClick={setCurrentPokemonId(pokemon.id)}/>
+                
+                );
+            })
+        }
+    </div>
+  )
+}
