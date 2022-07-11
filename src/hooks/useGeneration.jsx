2e674c032677e9e39be7a0c79d7f91b70470025b
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-07-11 23:10:53 +0530

    Fetch pokemons by generations

---
+++ src/hooks/useGeneration.jsx
@@ -0,0 +1,52 @@
+
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import generations from '../data/generations';
+import { fetchPokemonData,fetchPokemons } from '../api/api';
+
+
+
+export default function useGeneration(generationId) {
+    const [pokemons,setPokemons] = useState([]);
+    const [isLoading,setIsLoading] = useState(false);
+
+    const generation = useMemo(()=>{return generations.find((gen)=>gen.id===generationId)},[generationId])
+
+    // Fetch pokemons data by generation
+    const fetchData = useCallback(()=>{ 
+        if(generation.limit===null||generation.offset===null)
+        {
+            return;
+        }
+
+        setIsLoading(true);
+        setPokemons([]);
+
+        
+        // get all pokemons for the selected generation
+        fetchPokemons( generation.limit, generation.offset ).then( async ( { results } ) => {
+			const data = [];
+
+			// Get data for each specific Pokemon.
+			await Promise.all( results.map( async ( { name } ) => {
+				const pokemon = await fetchPokemonData( name );
+
+				data[ pokemon.id ] = pokemon;
+			} ) );
+
+			setPokemons( data );
+			setIsLoading( false );
+		} );
+	}, [] );
+
+    useEffect(()=>{
+        if(generationId){
+            fetchData();
+        }
+    },[generationId]);
+
+  return {
+    data: pokemons,
+    refetch: fetchData,
+    isLoading,
+  }
+}
